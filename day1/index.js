e1e8efd7b6a57964567460f742646151b5098957
Sejal Gupta <126438376+sejal966@users.noreply.github.com>
2025-08-26 10:17:14 +0530

    Add files via upload

---
+++ day1/index.js
@@ -0,0 +1,20 @@
+// const data = require("fs")
+// data.writeFileSync("abc.txt", "Hello")
+// const readData = data.readFileSync("abc.txt", "utf-8");
+// console.log(readData);
+
+// function sum(a,b){
+//     return a+b;
+// }
+// function sub(a,b){
+//     return a-b;
+// }
+
+// module.exports ={
+//     sum, sub
+// }
+
+// const data = require('chalk')
+// console.log(data.green("Hello"));
+// console.log(data.bgBlue("Hello"));
+
